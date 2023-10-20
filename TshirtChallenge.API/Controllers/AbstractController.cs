db84a61afbf692e24bf1a83e67e40da68efabdb0
vitor virges <vitor.virges@gmail.com>
2023-10-20 17:46:11 -0300

    feat: abstract controller and not found exception

---
+++ TshirtChallenge.API/Controllers/AbstractController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TshirtChallenge.Domain.Exceptions;
+
+namespace TshirtChallenge.API.Controllers
+{
+    public abstract class AbstractController : Controller
+    {
+        protected ActionResult HandleErrors(Exception ex)
+        {
+            if (ex is CustomValidationException)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            if (ex is NotFoundException)
+            {
+                return NotFound(ex.Message);
+            }
+
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+}
---
+++ TshirtChallenge.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,16 @@
+﻿namespace TshirtChallenge.Domain.Exceptions
+{
+    [Serializable]
+    public class NotFoundException : Exception
+    {
+        public NotFoundException() : base() { }
+
+        public NotFoundException(string message) : base(message) { }
+
+        public NotFoundException(string message, Exception inner) : base(message, inner) { }
+
+        protected NotFoundException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
